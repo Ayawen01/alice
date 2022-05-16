8aabe1d2fc67488b11ed4c26adf6798da1256fb6
ayawen <52957307+Ayawen01@users.noreply.github.com>
2022-05-16 23:10:50 +0800

    23:10

--- src/ast.rs
+++ src/ast.rs
@@ -1,5 +1,47 @@
-pub enum Expr {
+use crate::token::Token;
 
+pub enum Expr {
+    Grouping {
+        expression: Box<Expr>
+    },
+    Variable {
+        name: Token
+    },
+    Assign {
+        name: Token,
+        value: Box<Expr>
+    },
+    Unary {
+        operator: Token,
+        value: Box<Expr>
+    },
+    Binary {
+        left: Box<Expr>,
+        operator: Token,
+        right: Box<Expr>
+    },
+    Logical {
+        left: Box<Expr>,
+        operator: Token,
+        right: Box<Expr>  
+    },
+    Call {
+        callee: Box<Expr>,
+        paren: Token,
+        arguments: Vec<Expr>
+    },
+    Literal {
+        value: AliceLiteral
+    }
+}
+
+enum AliceLiteral {
+    Array(Vec<AliceLiteral>),
+    String(String),
+    F64(f64),
+    I64(i64),
+    Boolean(bool),
+    Nil
 }
 
 pub enum Stmt {
